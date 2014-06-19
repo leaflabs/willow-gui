8597a758175fbed2dc4e3276b3653369ab46d739
Chris Chronopoulos <chrono@leaflabs.com>
2014-06-19 17:25:49 -0400

    started building tools for error parsing

--- DebugTab.py
+++ DebugTab.py
@@ -1,4 +1,7 @@
 from PyQt4 import QtCore, QtGui
+import subprocess
+
+from parameters import *
 
 class DebugTab(QtGui.QWidget):
 
@@ -6,9 +9,37 @@ class DebugTab(QtGui.QWidget):
         super(DebugTab, self).__init__(None)
         self.parent = parent
 
-        self.placeholder = QtGui.QLabel('This tab will provide facilities to\nparse and clear error messages.')
+        self.debugButton = QtGui.QPushButton('Parse Error State')
+        self.debugButton.clicked.connect(self.parseErrors)
+
         self.layout = QtGui.QVBoxLayout()
-        self.layout.addWidget(self.placeholder)
+        self.layout.addWidget(self.debugButton)
         self.setLayout(self.layout)
 
+        self.moduleMap = {1:'central', 2:'sata', 3:'daq', 4:'udp', 5:'gpio'}
 
+    def parseErrors(self):
+        if self.parent.isDaemonRunning:
+            debugtool_po = subprocess.Popen([DAEMON_DIR+'util/debug_tool.py', 'read', 'error', '0'], stdout=subprocess.PIPE)
+            result = debugtool_po.stdout.readline()
+            start, stop = result.index('\t'), result.index('|')
+            bitfield = int(result[start:stop])
+            bitfield_str = bin(bitfield)[2:].zfill(8)
+            badModuleList = []
+            # lots of one-off confusion here, but i think this works now
+            for i in range(1,8):
+                if bitfield_str[-(i+1)]=='1':
+                    badModuleList.append(self.moduleMap[i])
+            error_msg = ''
+            for module in badModuleList:
+                debugtool_po = subprocess.Popen([DAEMON_DIR+'util/debug_tool.py', 'read', module, '0'], stdout=subprocess.PIPE)
+                result = debugtool_po.stdout.readline()
+                start, stop = result.index('\t'), result.index('|')
+                bitfield = int(result[start:stop])
+                bitfield_str = bin(bitfield)[2:].zfill(32)
+                for i in range(32):
+                    if bitfield_str[-(i+1)]=='1':
+                        pass # TODO append to error_msg here
+            #self.parent.statusBox.setText('Bad Modules: '+badModuleList)
+        else:
+            self.parent.statusBox.setText('Daemon is not running!')
