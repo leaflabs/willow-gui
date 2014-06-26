d813e77fd77427b538b94843b3ac7a24a5b5e3d5
Chris Chronopoulos <chrono@leaflabs.com>
2014-06-26 18:33:04 -0400

    added DAQ running checkbox

--- SetupTab.py
+++ SetupTab.py
@@ -12,27 +12,20 @@ class SetupTab(QtGui.QWidget):
         self.daemonCheckbox = QtGui.QCheckBox('Daemon Running')
         self.daemonCheckbox.stateChanged.connect(self.toggleDaemon)
 
+        self.daqCheckbox = QtGui.QCheckBox('DAQ Running')
+        self.daqCheckbox.stateChanged.connect(self.toggleDAQ)
+
         self.ethConfigButton = QtGui.QPushButton('Expand Ethernet Buffers')
         self.ethConfigButton.clicked.connect(self.configureEthernet)
 
         self.layout = QtGui.QVBoxLayout()
         self.layout.addWidget(self.ethConfigButton)
         self.layout.addWidget(self.daemonCheckbox)
+        self.layout.addWidget(self.daqCheckbox)
         self.layout.addSpacing(100)
         self.setLayout(self.layout)
 
-
-    def toggleDaemon(self):
-
-        if self.daemonCheckbox.isChecked():
-            subprocess.call([os.path.join(DAEMON_DIR, 'build/leafysd'), '-A', '192.168.1.2'])
-            self.parent.isDaemonRunning = True
-            self.parent.statusBox.append('Daemon started.')
-        else:
-            subprocess.call(['killall', 'leafysd'])
-            self.parent.isDaemonRunning = False
-            self.parent.statusBox.append('Daemon stopped.')
-        pass
+        self.acquireDotPy = os.path.join(DAEMON_DIR, 'util/acquire.py')
 
     def configureEthernet(self):
         """
@@ -43,9 +36,28 @@ class SetupTab(QtGui.QWidget):
         subprocess.call(os.path.join(DAEMON_DIR, 'util/expand_eth_buffers.sh'))
         self.parent.statusBox.append('Ethernet buffers expanded!')
 
-    def handleKeypress(self):
-        if self.keypressCheckbox.isChecked():
-            self.keypressCheckbox.setChecked(False)
+    def toggleDaemon(self):
+        if self.daemonCheckbox.isChecked():
+            subprocess.call([os.path.join(DAEMON_DIR, 'build/leafysd'), '-A', '192.168.1.2'])
+            self.parent.isDaemonRunning = True
+            self.parent.statusBox.append('Daemon started.')
         else:
-            self.keypressCheckbox.setChecked(True)
+            subprocess.call(['killall', 'leafysd'])
+            self.parent.isDaemonRunning = False
+            self.parent.statusBox.append('Daemon stopped.')
+        pass
+
+    def toggleDAQ(self):
+        if self.parent.isDaemonRunning:
+            if self.daqCheckbox.isChecked():
+                subprocess.call([self.acquireDotPy, 'start'])
+                self.parent.isDaqRunning = True
+                self.parent.statusBox.append('DAQ started.')
+            else:
+                subprocess.call([self.acquireDotPy, 'stop'])
+                self.parent.isDaqRunning = False
+                self.parent.statusBox.append('DAQ stopped.')
+        else:
+            self.parent.statusBox.append('Daemon is not running!')
+            self.daqCheckbox.setChecked(False) #TODO: gray this out if daemon not running
 
