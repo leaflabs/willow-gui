b0f8ac06a390e3148f141ebb938cd985b874fae6
Chris Chronopoulos <chrono@leaflabs.com>
2015-03-02 18:07:41 -0500

    added util/plotImpedance.py to quickly plot impedance measurements

---
+++ util/plotImpedance.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+import sys, time
+from PyQt4 import QtGui
+import numpy as np
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt4agg import NavigationToolbar2QTAgg as NavigationToolbar
+from matplotlib.figure import Figure
+
+class ImpedancePlotWindow(QtGui.QWidget):
+
+    def __init__(self, data):
+        QtGui.QWidget.__init__(self)
+        self.data = data
+
+        self.fig = Figure()
+        self.canvas = FigureCanvas(self.fig)
+        self.canvas.setParent(self)
+        self.toolbar = NavigationToolbar(self.canvas, self)
+        self.createPlot()
+
+        layout = QtGui.QVBoxLayout()
+        layout.addWidget(self.canvas)
+        layout.addWidget(self.toolbar)
+        self.setLayout(layout)
+        self.resize(1200,600)
+        self.setWindowTitle('Impedance Plot Window')
+        self.setWindowIcon(QtGui.QIcon('../img/round_logo_60x60.png'))
+        self.center()
+
+    def createPlot(self):
+        self.axes = self.fig.add_subplot(111)
+        self.axes.set_axis_bgcolor('k')
+        self.axes.plot(self.data, color='#8fdb90')
+        self.axes.set_title('Impedance at 1 kHz')
+        self.axes.set_xlabel('Channel Number')
+        self.axes.set_ylabel('Z (ohms)')
+        self.axes.set_xlim([0,1023])
+
+    def center(self):
+        fmgeo = self.frameGeometry()
+        currentScreen = QtGui.QApplication.desktop().screenNumber(QtGui.QApplication.desktop().cursor().pos())
+        centerPoint = QtGui.QApplication.desktop().screenGeometry(currentScreen).center()
+        fmgeo.moveCenter(centerPoint)
+        self.move(fmgeo.topLeft())
+
+if __name__=='__main__':
+    app = QtGui.QApplication(sys.argv)
+    filename = str(QtGui.QFileDialog.getOpenFileName(None, 'Select Impedance File', '../cal'))
+    if filename:
+        impedanceMeasurements = np.load(filename)
+        impedancePlotWindow = ImpedancePlotWindow(impedanceMeasurements)
+        impedancePlotWindow.show()
+        app.exec_()
