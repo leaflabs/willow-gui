054008fa9ad837157325cef75bd8e59672fa7008
Chris Chronopoulos <chronopoulos.chris@gmail.com>
2015-05-27 18:34:58 -0400

    main.py: only call mainWindow.exit() if mainWindow exists

--- src/main.py
+++ src/main.py
@@ -29,4 +29,5 @@ if __name__=='__main__':
         wizard = ConfigWizard()
         wizard.show()
         app.exec_()
-        wizard.main.exit()
+        if wizard.mainWindow:
+            wizard.mainWindow.exit()
