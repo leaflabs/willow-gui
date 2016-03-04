73f7cf967532106af33be319ae828a90a09282a5
Chris Chronopoulos <chronopoulos.chris@gmail.com>
2016-03-03 20:32:45 -0500

    SettingsWindow.py: made directory parameters into buttons which launch file browsers

--- src/SettingsWindow.py
+++ src/SettingsWindow.py
@@ -1,6 +1,22 @@
 from PyQt4 import QtCore, QtGui
 import config
 
+class DirectoryButton(QtGui.QPushButton):
+
+    def __init__(self, description, value):
+        QtGui.QPushButton.__init__(self, value)
+
+        self.value = value
+        self.description = description
+        self.clicked.connect(self.browseForDirectory)
+
+    def browseForDirectory(self):
+        filename = QtGui.QFileDialog.getExistingDirectory(self, ('Select %s' % self.description),
+                                                          self.value)
+        if filename:
+            self.setText(filename)
+
+
 class SettingsWindow(QtGui.QWidget):
 
     def __init__(self):
@@ -13,14 +29,23 @@ class SettingsWindow(QtGui.QWidget):
         self.saveButton.clicked.connect(self.save)
 
         layout = QtGui.QGridLayout()
-        self.lineEditMap = {}
+
+        self.valFieldMap = {}
         self.typeMap = {}
         for i,varName in enumerate(config.jsonDict.keys()):
-            lineEdit = QtGui.QLineEdit(str(config.jsonDict[varName]['value']))
-            self.lineEditMap[varName] = lineEdit
-            self.typeMap[varName] = type(config.jsonDict[varName]['value'])
-            layout.addWidget(QtGui.QLabel(config.jsonDict[varName]['description']), i,0)
-            layout.addWidget(lineEdit, i,1)
+            description = str(config.jsonDict[varName]['description'])
+            value = str(config.jsonDict[varName]['value'])
+            self.typeMap[varName] = type(config.jsonDict[varName]['value']) # shouldn't this be 'type'?
+            if (varName[-3:] == 'Dir'): # special treatment for directory parameters
+                layout.addWidget(QtGui.QLabel(description), i,0)
+                dirButton = DirectoryButton(description, value)
+                layout.addWidget(dirButton, i,1)
+                self.valFieldMap[varName] = dirButton
+            else:
+                layout.addWidget(QtGui.QLabel(description), i,0)
+                lineEdit = QtGui.QLineEdit(value)
+                layout.addWidget(lineEdit, i,1)
+                self.valFieldMap[varName] = lineEdit
         layout.addWidget(self.cancelButton, i+1,0)
         layout.addWidget(self.saveButton, i+1,1)
 
@@ -35,8 +60,8 @@ class SettingsWindow(QtGui.QWidget):
         self.close()
 
     def save(self):
-        for varName, lineEdit in self.lineEditMap.items():
-            config.jsonDict[varName]['value'] = self.typeMap[varName](lineEdit.text())
+        for varName, valField in self.valFieldMap.items():
+            config.jsonDict[varName]['value'] = self.typeMap[varName](valField.text())
         config.updateAttributes(config.jsonDict)
         config.saveJSON(config.jsonDict)
         self.close()
