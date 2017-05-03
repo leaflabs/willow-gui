15fafd11934fc845693b9df4cf4cc3a9d0e0764a
Chris Chronopoulos <chronopoulos.chris@gmail.com>
2017-05-02 23:37:23 -0400

    Add HeatMap.py
    Summary:
    HeatMap is a Qt Widget designed to display a rectangular grid of scalar
    values - for example, spiking activity per channel in chip, channel
    coordinates. Its interface includes a setActivity() method, as well as
    a dragAndDropSelected signal which emits x,y coordinates, plus a string
    provided by the widget which received the drop.
    
    Signed-off-by: Chris Chronopoulos <chrono@leaflabs.com>
    
    Test Plan:
    the widget can be run as a standalone application by simply
    running HeatMap.py as a script.
    
    Reviewers: perry, #willow
    
    Reviewed By: perry, #willow
    
    Differential Revision: https://reviews.leaflabs.com/D53

---
+++ src/HeatMap.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python2
+
+import sys
+from PyQt4 import QtGui, QtCore
+import numpy as np
+
+from viridis import viridis as cmap
+
+
+class HeatMap(QtGui.QLabel):
+
+    dragAndDropAccepted = QtCore.pyqtSignal(int, int, str)
+
+    def __init__(self, nrows, ncols, tileSize_init=10):
+        """
+        nrows: number of rows
+        ncols: number of columns
+        tileSize_init: initial side length of the square tiles (subject to
+            resize events in future versions, but locked to a square shape
+            for now)
+        """
+        QtGui.QLabel.__init__(self)
+
+        self.nrows = nrows
+        self.ncols = ncols
+
+        self.activity = np.zeros((self.nrows, self.ncols))
+
+        self.setMinimumSize(tileSize_init*self.ncols,tileSize_init*self.nrows)
+        self.setMaximumSize(tileSize_init*self.ncols, tileSize_init*self.nrows)
+
+    def setActivity(self, activity):
+        """
+        activity should be a (nrows,ncols) ndarray with values from 0 to 1
+        values outside this range will saturate the colormap
+        """
+        self.activity = activity
+        self.repaint()
+
+    def paintEvent(self, event):
+        painter = QtGui.QPainter(self)
+        pen = painter.pen()
+        pen.setStyle(QtCore.Qt.NoPen)
+        painter.setPen(pen)
+
+        for i in range(self.nrows):
+            for j in range(self.ncols):
+                x = j*self.rectw
+                y = i*self.recth
+                brush = QtGui.QBrush(QtGui.QColor(*tuple(cmap(self.activity[i,j]))))
+                painter.setBrush(brush)
+                pen = QtGui.QPen(QtGui.QColor(0,0,0), 1)
+                painter.setPen(pen)
+                painter.drawRect(x,y, self.rectw,self.recth)
+
+    def resizeEvent(self, event):
+        self.rectw = event.size().width() // self.ncols
+        self.recth = event.size().height() // self.nrows
+
+    def mousePressEvent(self, event):
+        if event.button() == QtCore.Qt.LeftButton:
+            self.hold = True
+            self.iselected = event.pos().x() // self.rectw
+            self.jselected = event.pos().y() // self.recth
+
+    def mouseMoveEvent(self, event):
+        if self.hold:
+            mimeData = QtCore.QMimeData()
+            drag = QtGui.QDrag(self)
+            drag.setMimeData(mimeData)
+            dropAction = drag.exec_()
+            if dropAction == QtCore.Qt.MoveAction:
+                dropText = str(drag.mimeData().text())
+                self.dragAndDropAccepted.emit(self.iselected, self.jselected, dropText)
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == QtCore.Qt.LeftButton:
+            self.hold = False
+
+if __name__ == '__main__':
+    # demo with random activity values
+    nrows, ncols = 16, 32
+    tileSize_init = 20
+    import random
+    app = QtGui.QApplication(sys.argv)
+    heatMap = HeatMap(nrows, ncols, tileSize_init)
+    activity = np.array([random.random() for i in range(nrows*ncols)]).reshape(nrows,ncols)
+    heatMap.setActivity(activity)
+    heatMap.show()
+    sys.exit(app.exec_())
