leaflabs/willow-gui

commit 97e28066416a79eb375f7065557f19bf4aada128

UI/style: replace "disk index" with "sample index"
Summary:
Stop calling what the willow-daemon calls "sample indices" "disk
indices" in the willow-gui UI and within the source code. Instead,
conform to the willow-daemon's terminology.

Test Plan: Does not affect functionality.

Reviewers: #willow, perry

Reviewed By: #willow, perry

Differential Revision: https://reviews.leaflabs.com/D262

src/StatusBar.py
@@ -13,7 +13,7 @@ BAD_STYLE = 'QLabel {background-color: orange; font: bold}'
 STREAM_STYLE = 'QLabel {background-color: rgb(0,191,255); font: bold}'
 RECORD_STYLE = 'QLabel {background-color: red; font: bold}'
 
-MAX_DISK_INDEX = 250e3*config.storageCapacity_GB
+MAX_SAMPLE_INDEX = 250e3*config.storageCapacity_GB
 DISK_FILLUP_FRACTION = 0.97
 
 
@@ -224,13 +224,13 @@ class StatusBar(QtGui.QWidget):
         tmp = vitals['record']
         if tmp == True:
             try:
-                diskIndex = hwif.getSataBSI()
-                if (diskIndex >= DISK_FILLUP_FRACTION*MAX_DISK_INDEX):
+                sampleIndex = hwif.getSataBSI()
+                if (sampleIndex >= DISK_FILLUP_FRACTION*MAX_SAMPLE_INDEX):
                     self.diskFillupDetected.emit()
-                self.recordLabel.setText('Recording: %5.2f%%' % (diskIndex/MAX_DISK_INDEX*100))
+                self.recordLabel.setText('Recording: %5.2f%%' % (sample_index/MAX_SAMPLE_INDEX*100))
                 self.recordLabel.setStyleSheet(RECORD_STYLE)
             except hwif.hwifError as e:
-                self.msgLog.post('StatusBar Error while trying to read disk index: %s' % e.message)
+                self.msgLog.post('StatusBar Error while trying to read sample index: %s' % e.message)
         elif tmp == False:
             self.recordLabel.setText('Not Recording')
             self.recordLabel.setStyleSheet(GOOD_STYLE)

src/ButtonPanel.py
@@ -237,8 +237,8 @@ class ButtonPanel(QtGui.QWidget):
     def handleDiskFillup(self):
         try:
             hwif.stopRecording()
-            diskIndex = hwif.getSataBSI()
-            self.msgLog.post('Disk fillup detected! Recording stopped at disk index %d' % diskIndex)
+            sampleIndex = hwif.getSataBSI()
+            self.msgLog.post('Disk fillup detected! Recording stopped at sample index %d' % sampleIndex)
         except hwif.AlreadyError:
             self.msgLog.post('Already not recording.')
         except hwif.hwifError as e:

commit 04a329227310b5641572681a2291febe35502911

StatusBar: fix typo in report of recording status
Summary:
Fix a typo (introduced in commit
97e28066416a79eb375f7065557f19bf4aada128) which raised an error while
attempting to report on disk fillup during recording.

Test Plan: start recording, observe lack of errors on WillowGUI's stderr

Reviewers: #willow, tasman

Reviewed By: tasman

Subscribers: tasman

Differential Revision: https://reviews.leaflabs.com/D302

src/StatusBar.py
@@ -227,7 +227,7 @@ class StatusBar(QtGui.QWidget):
                 sampleIndex = hwif.getSataBSI()
                 if (sampleIndex >= DISK_FILLUP_FRACTION*MAX_SAMPLE_INDEX):
                     self.diskFillupDetected.emit()
-                self.recordLabel.setText('Recording: %5.2f%%' % (sample_index/MAX_SAMPLE_INDEX*100))
+                self.recordLabel.setText('Recording: %5.2f%%' % (sampleIndex/MAX_SAMPLE_INDEX*100))
                 self.recordLabel.setStyleSheet(RECORD_STYLE)
             except hwif.hwifError as e:
                 self.msgLog.post('StatusBar Error while trying to read sample index: %s' % e.message)