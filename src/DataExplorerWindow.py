972e11c354045f466ac9caf24a047f9487d674de
Sara Sinback <sara.sinback@gmail.com>
2017-07-20 16:19:49 -0400

    DataExplorer: add help menu, activated by F1
    Summary:
    Add a help menu describing basic user interaction with the Data
    Explorer. Make this help menu be launchable from the Data Explorer with
    the F1 key.
    
    Test Plan:
    launch Data Explorer, press F1, move window around or close
    it, press F1 some more...
    
    Reviewers: #willow, cblam
    
    Reviewed By: #willow, cblam
    
    Subscribers: #willow
    
    Differential Revision: https://reviews.leaflabs.com/D387

--- src/DataExplorerWindow.py
+++ src/DataExplorerWindow.py
@@ -10,6 +10,38 @@ from TimeScrubber import TimeScrubber
 
 from WillowDataset import WillowDataset
 
+class HelpWindow(QtGui.QDialog):
+    def __init__(self, *args, **kwargs):
+        QtGui.QDialog.__init__(self, *args, **kwargs)
+
+        self.setWindowTitle('DataExplorer Help')
+
+        help_texts = []
+
+        help_texts.append(QtGui.QLabel('The WillowGUI Data Explorer allows inspection of snapshot or experiment\n'
+            'data recorded with a Willow. The colorful heat map in the upper-left corner is\n'
+            'filled with squares which represent the 32 different ADC chips connected to the\n'
+            'Willow during the recording, and each of their 32 individual recording channels.\n'
+            'To inspect one of these channels more closely, click on a colored square and drag\n'
+            'into one of the eight graphs in the bottom part of the GUI. You can adjust the\n'
+            'length of time inspected with the time scrubber on the top left.\n\n'))
+        help_texts.append(QtGui.QLabel('Keyboard shortcuts:\n'))
+        font = QtGui.QFont()
+        font.setBold(True)
+        help_texts[-1].setFont(font)
+        help_texts.append(QtGui.QLabel('F1 - launch this help menu\n'
+            'F - toggles filtering of displayed data\n'
+            'F11 - toggle fullscreen viewing of the Data Explorer\n'
+            'Home - reset viewing range of the eight plots on the bottom\n'
+            'Scrollwheel - zoom x and y dimensions of plots\n'
+            'Ctrl+Scrollwheel - zoom only x dimension of plots\n'
+            'Shift+Scrollwheel - zoom only y dimension of plots\n'))
+        layout = QtGui.QVBoxLayout()
+        for help_text in help_texts:
+            layout.addWidget(help_text)
+        self.setLayout(layout)
+
+
 class DataExplorerWindow(QtGui.QWidget):
 
     def __init__(self, filename):
@@ -54,6 +86,8 @@ class DataExplorerWindow(QtGui.QWidget):
 
         self.filtered = False
 
+        self.helpWindow = HelpWindow(parent=self)
+
         self.timeScrubber.bang()
 
     def handleTimeSelection(self, start, stop):
@@ -94,6 +128,16 @@ class DataExplorerWindow(QtGui.QWidget):
             if willowChan:
                 self.setPlotChan(subplotIndex, willowChan)
 
+    def showHelp(self):
+        if self.helpWindow.isHidden():
+            self.helpWindow.setGeometry(QtCore.QRect(100,100,500,250))
+            self.helpWindow.show()
+        else:
+            # focus window which is already somewhere on desktop
+            self.helpWindow.setWindowState(self.helpWindow.windowState() &
+                ~QtCore.Qt.WindowMinimized | QtCore.Qt.WindowActive)
+        self.helpWindow.activateWindow()
+
     def keyPressEvent(self, event):
         if event.key() == QtCore.Qt.Key_F11:
             if self.windowState() & QtCore.Qt.WindowFullScreen:
@@ -107,6 +151,8 @@ class DataExplorerWindow(QtGui.QWidget):
             self.updateAllPlots()
         elif event.key() == QtCore.Qt.Key_Home:
             self.plotMatrix.home()
+        elif event.key() == QtCore.Qt.Key_F1:
+            self.showHelp()
 
 if __name__=='__main__':
     import sys
